e191638c02cd0ff467a5ef6d759238c0d80cba4c
8thMay03 <meoto142@gmail.com>
2023-07-09 13:53:04 +0700

    code

--- DSA03014.SoKhoiLapPhuong.cpp
+++ DSA03014.SoKhoiLapPhuong.cpp
@@ -1,29 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 #define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define fi first
+#define se second
+#define pb push_back
+#define pii pair<int,int>
 
 using namespace std;
 
-
 vector<ll> v;
 
-bool find(ll k,string s){         //Tìm xem số k có ở trong xâu s hay không
-    string tmp=to_string(k);
-    map<char,int> mp;
-    ll i=0,j=0;
-    for(char c:tmp) mp[c]++;                    // Đánh dấu số lần xuất hiện của từng số của k
-    while(i<=tmp.size() && j<=s.size()){         // Duyệt k và xâu s, nếu có 2 kí tự giống nhau thì trừ số lần xuất hiện đi
-        if(tmp[i]==s[j]){                           
-            mp[tmp[i]]--;
-            i++;j++;
-        }
-        else j++;
+bool find(ll k,string s){           //Tìm xem số k có ở trong xâu s không theo đúng thứ tự
+    string x=to_string(k);
+    int i=0,j=0;
+    while(i<x.size() and j<s.size()){   //Tìm từ trái qua phải
+        if(x[i]!=s[j]) j++;
+        else i++,j++;
     }
-    for(char c:tmp) if(mp[c]) return false;     // Sau khi duyệt, nếu số k xuất hiện trong xâu thì số lần xuất hiện của các số đã bị trừ về 0, ngược lại thì return false
-    return true;
+    if(i==x.size()) return true;
+    return false;
 }
- 
+
 int main(){
-    for(ll i=1e2;i>=0;i--) v.push_back(i*i*i);
+    for(ll i=1e2;i>=0;i--) v.pb(i*i*i);
     int t;
     cin >> t;
     while(t--){
