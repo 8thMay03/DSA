dd762d383e3c2649f7bc83234005c4181e970e9c
8thMay03 <123087852+8thMay03@users.noreply.github.com>
2024-05-23 23:13:06 +0700

    Update DSA06031.PhanTuLonNhatTrongDayCon.cpp

--- DSA06031.PhanTuLonNhatTrongDayCon.cpp
+++ DSA06031.PhanTuLonNhatTrongDayCon.cpp
@@ -1,24 +1,46 @@
 #include <bits/stdc++.h>
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vvi vector<vi>
+#define vvl vector<vll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define mii map<int, int>
+#define mll map<ll, ll>
+#define faster() ios::sync_with_stdio(false); cin.tie(0);
+#define pi 3.14159265358979323846
+
 using namespace std;
-int n,a[1005];
-bool check(){
-    for(int i=0;i<n-1;i++){
-        if(a[i]>a[i+1]) return 0;
+
+template <typename T> istream& operator >> (istream& in, vector<T>& vec) {for (T& element : vec) in >> element; return in;}
+template <typename T> ostream& operator << (ostream& out, vector<T>& vec) {for (T& element : vec) out << element << " "; return out;}
+template <typename T> ostream& operator << (ostream& out, set<T>& st) {for (const T& element : st) out << element << " "; return out;}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    cin >> a;
+    for (int i = k - 1; i < n; i++) {
+        cout << *max_element(a.begin() + i - k + 1, a.begin() + i + 1) << " ";
     }
-    return 1;
+    cout << endl;
 }
 
-int main(){
-    int t;
+int main() {
+    faster();
+    int t = 1;
     cin >> t;
-    while(t--){
-        int n,k;
-        cin >> n >> k;
-        int a[n];
-        for(int i=0;i<n;i++) cin >> a[i];
-        for(int i=0;i<n-k+1;i++){
-            cout << *max_element(a+i,a+i+k) <<" ";
-        }
-        cout << endl;
+    while (t--) {
+        solve();
     }
 }
