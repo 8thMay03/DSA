96db6bf0ea07d05e71f36dd10b27b9e4bc70483d
8thMay03 <123087852+8thMay03@users.noreply.github.com>
2024-05-23 23:09:24 +0700

    Update DSA06033.KhoangCachXaNhat.cpp

--- DSA06033.KhoangCachXaNhat.cpp
+++ DSA06033.KhoangCachXaNhat.cpp
@@ -1,24 +1,55 @@
 #include <bits/stdc++.h>
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vvi vector<vi>
+#define vvl vector<vll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define mii map<int, int>
+#define mll map<ll, ll>
+#define faster() ios::sync_with_stdio(false); cin.tie(0);
+#define pi 3.14159265358979323846
+
 using namespace std;
 
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        int n,res=-1;
-        cin >> n;
-        int a[n],rmax[n]; 
-        for(int i=0;i<n;i++) cin >> a[i];
-        rmax[n-1]=a[n-1];
-        for(int i=n-2;i>=0;i--) rmax[i]=max(rmax[i+1],a[i]);
-        int i=0,j=0;
-        while(i<n && j<n){
-            if(a[i]<rmax[j]){
-                res=max(res,j-i);
-                j++;
-            }
-            else i++;
+template <typename T> istream& operator >> (istream& in, vector<T>& vec) {for (T& element : vec) in >> element; return in;}
+template <typename T> ostream& operator << (ostream& out, vector<T>& vec) {for (T& element : vec) out << element << " "; return out;}
+template <typename T> ostream& operator << (ostream& out, set<T>& st) {for (const T& element : st) out << element << " "; return out;}
+
+void solve() {
+    int n, res = -1;
+    cin >> n;
+    vector<int> a(n), rmax(n); 
+    cin >> a;
+    rmax[n - 1] = a[n - 1];
+    for (int i = n - 2; i >= 0; i--) {
+        rmax[i] = max(rmax[i + 1], a[i]);
+    }
+    int i = 0, j = 0;
+    while (i < n && j < n) {
+        if(a[i] < rmax[j]) {
+            res = max(res, j - i);
+            j++;
         }
-        cout << res << endl;    
+        else i++;
+    }
+    cout << res << endl;    
+}
+
+int main() {
+    faster();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
     }
 }
