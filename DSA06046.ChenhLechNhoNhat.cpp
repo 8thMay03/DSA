9f8210e8ccaa3c68d677920b0322501b4c6cf785
8thMay03 <123087852+8thMay03@users.noreply.github.com>
2024-05-23 23:19:40 +0700

    Update DSA06046.ChenhLechNhoNhat.cpp

--- DSA06046.ChenhLechNhoNhat.cpp
+++ DSA06046.ChenhLechNhoNhat.cpp
@@ -1,19 +1,48 @@
 #include <bits/stdc++.h>
+
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vvi vector<vi>
+#define vvl vector<vll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define mii map<int, int>
+#define mll map<ll, ll>
+#define faster() ios::sync_with_stdio(false); cin.tie(0);
+#define pi 3.14159265358979323846
+
 using namespace std;
 
-int main(){
-    int t;
+template <typename T> istream& operator >> (istream& in, vector<T>& vec) {for (T& element : vec) in >> element; return in;}
+template <typename T> ostream& operator << (ostream& out, vector<T>& vec) {for (T& element : vec) out << element << " "; return out;}
+template <typename T> ostream& operator << (ostream& out, set<T>& st) {for (const T& element : st) out << element << " "; return out;}
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    cin >> a;
+    sort(all(a));
+    int ans = INT_MAX;
+    for (int i = 0; i < n - 1; i++) {
+        ans = min(ans, a[i + 1] - a[i]);
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    faster();
+    int t = 1;
     cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        int a[n];
-        for(int i=0;i<n;i++) cin >> a[i];
-        sort(a,a+n);
-        int res=1e9;
-        for(int i=0;i<n-1;i++){
-            res=min(res,abs(a[i]-a[i+1]));
-        }
-        cout << res << endl;
+    while (t--) {
+        solve();
     }
 }
