a3ba1ae31ee14526c1f7c70cab6666db64660363
8thMay03 <123087852+8thMay03@users.noreply.github.com>
2024-05-25 21:47:37 +0700

    Update DSA05018.XauConDoiXungDaiNhat.cpp

--- DSA05018.XauConDoiXungDaiNhat.cpp
+++ DSA05018.XauConDoiXungDaiNhat.cpp
@@ -1,36 +1,34 @@
-#include <bits/stdc++.h>
-#define ll long long
+#include<bits/stdc++.h>
 
 using namespace std;
 
-int dp[1005][1005]={0};
-
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        memset(dp,0,sizeof(dp));
-        int res=1;
-        string s;
-        cin >> s;
-        for(int i=0;i<s.size();i++) dp[i][i]=1;
-        for(int l=2;l<=s.size();l++){
-            for(int i=0;i<=s.size()-l;i++){
-                int j=i+l-1;
-                if(j-i==1){
-                    if(s[i]==s[j]){
-                        dp[i][j]=1;
-                        res=max(res,l);
-                    }
-                }
-                else if(s[i]==s[j]){
-                    if(dp[i+1][j-1]){
-                        dp[i][j]=1;
-                        res=max(res,l);
-                    }
-                }
-            }
-        }
-        cout << res << endl;
-    }
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		string s;
+		cin >> s;
+		vector<vector<bool>> dp(s.size() + 1, vector<bool>(s.size() + 1, false));
+		for (int i = 0; i < s.size(); i++) {
+			dp[i][i] = true;
+		}
+		int ans = 1;
+		for (int l = 2; l <= s.size(); l++) {
+			for (int i = 0; i + l <= s.size(); i++) {
+				int j = i + l - 1;
+				if (j - i == 1) {
+					if (s[i] == s[j]) {
+						dp[i][j] = true;
+						ans = max(ans, l);
+					}
+				} else if (s[i] == s[j]) {
+					if (dp[i + 1][j - 1]) {
+						dp[i][j] = true;
+						ans = max(ans, l);
+					}
+				}
+			}
+		}
+		cout << ans << endl;
+	}
 }
