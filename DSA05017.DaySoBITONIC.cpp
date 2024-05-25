5b49032ee8dcbb138eb853272622dad62ae4fa6d
8thMay03 <123087852+8thMay03@users.noreply.github.com>
2024-05-25 11:13:08 +0700

    Update DSA05017.DaySoBITONIC.cpp

--- DSA05017.DaySoBITONIC.cpp
+++ DSA05017.DaySoBITONIC.cpp
@@ -1,34 +1,64 @@
 #include <bits/stdc++.h>
+
 #define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vvi vector<vi>
+#define vvl vector<vll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define mii map<int, int>
+#define mll map<ll, ll>
+#define faster() ios::sync_with_stdio(false); cin.tie(0);
+#define pi 3.14159265358979323846
 
 using namespace std;
 
+template <typename T> istream& operator >> (istream& in, vector<T>& vec) {for (T& element : vec) in >> element; return in;}
+template <typename T> ostream& operator << (ostream& out, vector<T>& vec) {for (T& element : vec) out << element << " "; return out;}
+template <typename T> ostream& operator << (ostream& out, set<T>& st) {for (const T& element : st) out << element << " "; return out;}
 
-int main(){
-    int t;
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    cin >> a;
+    int ans = 0;
+    vector<int> dp1(n, 0), dp2(n, 0);
+    for (int i = 0; i < n; i++) {
+        dp1[i] += a[i];
+        for (int j = 0; j < i; j++) {
+            if (a[i] > a[j]) {
+                dp1[i] = max(dp1[i], dp1[j] + a[i]);
+            }
+        }
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        dp2[i] += a[i];
+        for (int j = n - 1; j > i; j--) {
+            if (a[i] > a[j]) {
+                dp2[i] = max(dp2[i], dp2[j] + a[i]);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        ans = max(ans, dp1[i] + dp2[i] - a[i]);
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    faster();
+    int t = 1;
     cin >> t;
-    while(t--){
-        int n,res=0;
-        cin >> n;
-        int a[n],dp1[n],dp2[n];
-        for(int i=0;i<n;i++){
-            cin >> a[i];
-            dp1[i]=a[i];
-            dp2[i]=a[i];
-        }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(a[i]>a[j]) dp1[i]=max(dp1[i],dp1[j]+a[i]);
-            }
-        }
-        for(int i=n-2;i>=0;i--){
-            for(int j=n-1;j>i;j--){
-                if(a[j]<a[i]) dp2[i]=max(dp2[i],dp2[j]+a[i]);
-            }
-        }
-        for(int i=0;i<n;i++){
-            res=max(res,dp1[i]+dp2[i]-a[i]);
-        }
-        cout << res << endl;
+    while (t--) {
+        solve();
     }
 }
