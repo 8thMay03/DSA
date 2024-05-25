d0ae2fd8e860d9555e543d40949260a344e6f962
8thMay03 <123087852+8thMay03@users.noreply.github.com>
2024-05-25 10:07:26 +0700

    Update DSA06049.DemCap.cpp

--- DSA06049.DemCap.cpp
+++ DSA06049.DemCap.cpp
@@ -1,20 +1,50 @@
 #include <bits/stdc++.h>
-using namespace std;
-#define ll long long
 
-int main(){
-    int t;
+#define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vvi vector<vi>
+#define vvl vector<vll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define mii map<int, int>
+#define mll map<ll, ll>
+#define faster() ios::sync_with_stdio(false); cin.tie(0);
+#define pi 3.14159265358979323846
+
+using namespace std;
+
+template <typename T> istream& operator >> (istream& in, vector<T>& vec) {for (T& element : vec) in >> element; return in;}
+template <typename T> ostream& operator << (ostream& out, vector<T>& vec) {for (T& element : vec) out << element << " "; return out;}
+template <typename T> ostream& operator << (ostream& out, set<T>& st) {for (const T& element : st) out << element << " "; return out;}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    cin >> a;
+    sort(all(a));
+    ll ans = 0;
+    for (int i = 0; i < n - 1; i++) {
+        int it = lower_bound(a.begin() + i + 1, a.end(), k + a[i]) - a.begin();
+        it--;
+        ans += it - i;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    faster();
+    int t = 1;
     cin >> t;
-    while(t--){
-        ll n,k,cnt=0;
-        cin >> n >> k;
-        int a[n];
-        for(int i=0;i<n;i++) cin >> a[i];
-        sort(a,a+n);
-        for(int i=0;i<n-1;i++){
-           auto it=lower_bound(a+i+1,a+n,k+a[i]);       //Do aj-ai < k nên ta tìm vị trí j đầu tiên mà aj-ai>=k, các phần tử phía trước sẽ thỏa mãn aj-ai<k.
-           cnt+=it-a-i-1;
-        }
-        cout << cnt << endl;
+    while (t--) {
+        solve();
     }
 }
