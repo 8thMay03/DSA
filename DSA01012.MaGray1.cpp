d081b19e4350566a1c68627360618c951feb1b28
8thMay03 <123087852+8thMay03@users.noreply.github.com>
2024-05-25 15:49:01 +0700

    Update DSA01012.MaGray1.cpp

--- DSA01012.MaGray1.cpp
+++ DSA01012.MaGray1.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
- 
+
 #define ll long long
 #define vi vector<int>
 #define vll vector<long long>
@@ -13,39 +13,40 @@
 #define se second
 #define pb push_back
 #define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
 #define mii map<int, int>
 #define mll map<ll, ll>
 #define faster() ios::sync_with_stdio(false); cin.tie(0);
- 
+#define pi 3.14159265358979323846
+
 using namespace std;
- 
-template <typename T> istream& operator>>(istream& in, vector<T>& vec){for (T& element : vec) in >> element; return in;}
-template <typename T> ostream& operator<<(ostream& out, vector<T>& vec){for (T& element : vec) out << element << endl; return out;}
-template <typename T> ostream& operator<<(ostream& out, set<T>& st){for (const T& element : st) out << element << " "; return out;}
- 
-int main(){
-    faster();
-    int t;
-    cin >> t;
-    while(t--){
-        int n;
-        cin >> n;
-        if(n == 1) cout << 0 << endl << 1;
-        else{
-            n--;
-            vs gray;
-            gray.pb("0");
-            gray.pb("1");
-            while(n--){
-                vs tmp = gray;
-                for(int i = 0; i < (int)gray.size(); i++){
-                    gray[i] = "0" + gray[i];
-                }
-                reverse(all(tmp));
-                for(string s: tmp) gray.pb("1" + s);
-            }
-            cout << gray;
+
+template <typename T> istream& operator >> (istream& in, vector<T>& vec) {for (T& element : vec) in >> element; return in;}
+template <typename T> ostream& operator << (ostream& out, vector<T>& vec) {for (T& element : vec) out << element << " "; return out;}
+template <typename T> ostream& operator << (ostream& out, set<T>& st) {for (const T& element : st) out << element << " "; return out;}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<string> gray = {"0", "1"};
+    n--;
+    while (n--) {
+        vector<string> temp = gray;
+        reverse(all(temp));
+        for (int i = 0; i < gray.size(); i++) {
+            gray[i] = "0" + gray[i];
+            temp[i] = "1" + temp[i];
         }
-        cout << endl;
+        gray.insert(gray.end(), all(temp));
+    }
+    cout << gray << endl;
+}
+
+int main() {
+    faster();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
     }
 }
