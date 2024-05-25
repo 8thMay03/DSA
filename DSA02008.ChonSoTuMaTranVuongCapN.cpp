58f64202b3142b8d5422e23151374680ea385b48
8thMay03 <123087852+8thMay03@users.noreply.github.com>
2024-05-25 10:36:14 +0700

    Update DSA02008.ChonSoTuMaTranVuongCapN.cpp

--- DSA02008.ChonSoTuMaTranVuongCapN.cpp
+++ DSA02008.ChonSoTuMaTranVuongCapN.cpp
@@ -1,37 +1,74 @@
 #include <bits/stdc++.h>
+
 #define ll long long
+#define vi vector<int>
+#define vll vector<long long>
+#define vc vector<char>
+#define vs vector<string>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vvi vector<vi>
+#define vvl vector<vll>
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define mii map<int, int>
+#define mll map<ll, ll>
+#define faster() ios::sync_with_stdio(false); cin.tie(0);
+#define pi 3.14159265358979323846
 
 using namespace std;
 
-int n,k,a[1005][1005],tmp=0,col[1005]={0};
-vector<int> v;
-vector<vector<int>> vv;
+template <typename T> istream& operator >> (istream& in, vector<T>& vec) {for (T& element : vec) in >> element; return in;}
+template <typename T> ostream& operator << (ostream& out, vector<T>& vec) {for (T& element : vec) out << element << " "; return out;}
+template <typename T> ostream& operator << (ostream& out, set<T>& st) {for (const T& element : st) out << element << " "; return out;}
 
-void Try(int i){
-    for(int j=1;j<=n;j++){
-        if(!col[j]){
-            col[j]=1;
+void Try(int i, vector<vector<int>> a, vector<int> v, vector<int>& vis, vector<vector<int>>& ans, int sum, int k) {
+    for (int j = 0; j < a.size(); j++) {
+        if (!vis[j]) {
+            sum += a[i][j];
+            vis[j] = 1;
             v.push_back(j);
-            tmp+=a[i][j];
-            if(tmp==k && v.size()==n) vv.push_back(v);
-            else if(tmp<k) Try(i+1);
-            tmp-=a[i][j];
-            col[j]=0;
+            if (i == a.size() - 1) {
+                if (sum == k) {
+                    ans.push_back(v);
+                }
+            } else {
+                Try(i + 1, a, v, vis, ans, sum, k);
+            }
             v.pop_back();
+            vis[j] = 0;
+            sum -= a[i][j];
         }
     }
 }
-int main(){
-   cin >> n >> k;
-   for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cin >> a[i][j];
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<vector<int>> a(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    vector<vector<int>> ans;
+    vector<int> vis(n, 0);
+    Try(0, a, {}, vis, ans, 0, k);
+    cout << ans.size() << endl;
+    for (int i = 0; i < ans.size(); i++) {
+        for (int j = 0; j < ans[i].size(); j++) {
+            cout << ans[i][j] + 1 << " ";
         }
-   }
-   Try(1);
-   cout << vv.size() << endl;
-   for(auto v:vv){
-        for(auto i:v) cout << i <<" ";
         cout << endl;
-   }  
+    }
+}
+
+int main() {
+    faster();
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
 }
